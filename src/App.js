06f4bf841e2911989208ade1cde83af252689328
giemaz <ghaleonlt@google.com>
2023-03-03 15:41:15 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import './App.css';
 
 function App() {
 	const [jokes, setJokes] = useState([]);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		async function fetchJokes() {
 			let fetchedJokes = [];
 			while (fetchedJokes.length < 10) {
-				const response = await fetch('https://v2.jokeapi.dev/joke/Programming');
-				const data = await response.json();
-				if (data.joke) {
-					let hasSetup = true;
-					let setup = data.setup;
-					let delivery = data.delivery;
-					let tags = 'twopart';
-					if (data.type === 'single') {
-						hasSetup = false;
-						setup = data.joke;
-						delivery = '';
-						tags = 'single';
+				try {
+					const response = await fetch('https://v2.jokeapi.dev/joke/Programming');
+					const data = await response.json();
+					if (data.joke) {
+						let hasSetup = true;
+						let setup = data.setup;
+						let delivery = data.delivery;
+						let tags = 'twopart';
+						if (data.type === 'single') {
+							hasSetup = false;
+							setup = data.joke;
+							delivery = '';
+							tags = 'single';
+						}
+						fetchedJokes.push({
+							setup,
+							delivery,
+							hasSetup,
+							tags,
+						});
 					}
-					fetchedJokes.push({
-						setup,
-						delivery,
-						hasSetup,
-						tags,
-					});
+				} catch (error) {
+					setError('Error fetching jokes.');
+					break;
 				}
 			}
 			setJokes(fetchedJokes);
@@ -37,29 +43,32 @@ function App() {
 	return (
 		<div className='container'>
 			<h1>Jokes-On-You</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>No.</th>
-						<th>Tags</th>
-						<th>Joke</th>
-					</tr>
-				</thead>
-				<tbody>
-					{jokes.map((joke, index) => (
-						<tr key={index}>
-							<td>{index + 1}</td>
-							<td>
-								{joke.hasSetup ? 'Setup' : 'Joke'} {joke.hasSetup && ' / Delivery'}
-							</td>
-							<td>
-								{joke.hasSetup && <span className='tag'>setup:</span>} {joke.setup}{' '}
-								{joke.hasSetup && <span className='tag'>delivery:</span>} {joke.delivery}
-							</td>
+			{error && <p className='error'>{error}</p>}
+			{!error && (
+				<table>
+					<thead>
+						<tr>
+							<th>No.</th>
+							<th>Tags</th>
+							<th>Joke</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{jokes.map((joke, index) => (
+							<tr key={index}>
+								<td>{index + 1}</td>
+								<td>
+									{joke.hasSetup ? 'Setup' : 'Joke'} {joke.hasSetup && ' / Delivery'}
+								</td>
+								<td>
+									{joke.hasSetup && <span className='tag'>setup:</span>} {joke.setup}{' '}
+									{joke.hasSetup && <span className='tag'>delivery:</span>} {joke.delivery}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)}
 		</div>
 	);
 }
